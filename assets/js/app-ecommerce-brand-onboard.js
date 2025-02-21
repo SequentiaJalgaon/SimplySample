67f6001c582bcb4b39ccc971b1549a7e3ac984c2
Sequentia Software Solutions PVT LTD <133221563+SequentiaJalgaon@users.noreply.github.com>
2025-02-21 10:19:53 +0530

    Brand - Image Upload

--- assets/js/app-ecommerce-brand-onboard.js
+++ assets/js/app-ecommerce-brand-onboard.js
@@ -41,3 +41,98 @@
 
   
 })();
+
+    (function () {
+            'use strict'
+            
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            var forms = document.querySelectorAll('.needs-validation')
+            
+            // Loop over them and prevent submission
+            Array.prototype.slice.call(forms)
+              .forEach(function (form) {
+                form.addEventListener('submit', function (event) {
+                  // console.log(form.checkValidity());
+                  if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                  } else {
+                    myFunction(event);
+                  }
+                  
+                  form.classList.add('was-validated')
+                }, true)
+              })
+    })()
+    
+     async function myFunction(event, $brand_id) {
+         event.preventDefault();
+          event.stopPropagation();
+
+          const uploadedImagesbase64 = [];
+          const imagesSaved = [];
+          var uploadedImages = document.querySelectorAll('[data-dz-thumbnail]');
+          if(uploadedImages.length == 0 ) {
+            event.target.classList.add('was-validated');
+          } else {
+            uploadedImages.forEach((item, index, arr) => {
+              var base64image = uploadedImages[index].src;
+              if(base64image != "") {
+                
+                var currentdate = new Date();
+                var imageName = 
+                currentdate.getFullYear() + ""  
+                + (currentdate.getMonth()+1)  + "" 
+                + currentdate.getDate() + ""
+                + currentdate.getHours() + ""  
+                + currentdate.getMinutes() + "" 
+                + currentdate.getSeconds()
+                + "_" + index.toString() + ".png";
+
+                uploadedImagesbase64.push(
+                  {  name : imageName , base64Data: base64image }
+                );
+
+                imagesSaved.push(imageName);
+
+              }
+            })
+          }
+
+        var brandLogo = {
+            images: uploadedImagesbase64,
+            brand_id : $brand_id
+          };
+        const promise = fetch(ImgaeUploadBrandAPI, {
+            method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ brandLogo }),
+            });
+
+        promise
+        .then(response => response.json())
+        .then(data => {
+            
+            let uploadResult = true;
+            if (typeof data.status !== 'undefined' && data.status == 'success' && data.responses.length > 0) 
+            {
+                data.responses.forEach((p) => { if(p.status != "success") uploadResult = uploadResult == true ? false : uploadResult;  });
+
+                if(uploadResult == true) {
+                    
+                    //   document.querySelector('form button[type="submit"]').click();
+
+                    // return true;
+                    
+                 } else {
+                    //   return false;
+                    
+                 }
+            }
+        })
+
+     }
+  
+  
